74505b54450ebb02e463fd65cef29ab2fb4c5649
karim ashroff <karimashroff@gmail.com>
2023-08-11 18:46:50 -0400

    Added all symbols and names of the Elements

--- assets/elements.js
+++ assets/elements.js
@@ -106,5 +106,251 @@ export const elements = {
     },
     "Kr": {
         "name": "Krypton"
+    },
+    "Rb": {
+        "name": "Rubidium"
+    },
+    "Sr": {
+        "name": "Strontium"
+    },
+    "Y": {
+        "name": "Yttrium"
+    },
+    "Zr": {
+        "name": "Zirconium"
+    },
+    "Nb": {
+        "name": "Niobium"
+    },
+    "Mo": {
+        "name": "Molybdenum"
+    },
+    "Tc": {
+        "name": "Technetium"
+    },
+    "Ru": {
+        "name": "Ruthenium"
+    },
+    "Rh": {
+        "name": "Rhodium"
+    },
+    "Pd": {
+        "name": "Palladium"
+    },
+    "Ag": {
+        "name": "Silver"
+    },
+    "Cd": {
+        "name": "Cadmium"
+    },
+    "In": {
+        "name": "Indium"
+    },
+    "Sn": {
+        "name": "Tin"
+    },
+    "Sb": {
+        "name": "Atimony"
+    },
+    "Te": {
+        "name": "Tellurium"
+    },
+    "I": {
+        "name": "Iodine"
+    },
+    "Xe": {
+        "name": "Xenon"
+    },
+    "Cs": {
+        "name": "Caesium"
+    },
+    "Ba": {
+        "name": "Barium"
+    },
+    "Lu": {
+        "name": "Lutetium"
+    },
+    "Hf": {
+        "name": "Hafnium"
+    },
+    "Ta": {
+        "name": "Tantaum"
+    },
+    "W": {
+        "name": "Tungsten"
+    },
+    "Re": {
+        "name": "Rhenium"
+    },
+    "Os": {
+        "name": "Osmium"
+    },
+    "Ir": {
+        "name": "Iridium"
+    },
+    "Pt": {
+        "name": "Platinum"
+    },
+    "Au": {
+        "name": "Gold"
+    },
+    "Hg": {
+        "name": "Mercury"
+    },
+    "Tl": {
+        "name": "Thallium"
+    },
+    "Pb": {
+        "name": "Lead"
+    },
+    "Bi": {
+        "name": "Bismuth"
+    },
+    "Po": {
+        "name": "Polonium"
+    },
+    "At": {
+        "name": "Astatine"
+    },
+    "Rn": {
+        "name": "Radon"
+    },
+    "Fr": {
+        "name": "Francium"
+    },
+    "Ra": {
+        "name": "Radium"
+    },
+    "Lr": {
+        "name": "Lawrencium"
+    },
+    "Rf": {
+        "name": "Rutherfordium"
+    },
+    "Db": {
+        "name": "Dubnium"
+    },
+    "Sg": {
+        "name": "Seaborgium"
+    },
+    "Bh": {
+        "name": "Bohrium"
+    },
+    "Hs": {
+        "name": "Hassium"
+    },
+    "Mt": {
+        "name": "Meitnerium"
+    },
+    "Ds": {
+        "name": "Darmstadtium"
+    },
+    "Rg": {
+        "name": "Roentgenium"
+    },
+    "Cn": {
+        "name": "Copernicium"
+    },
+    "Nh": {
+        "name": "Nihonium"
+    },
+    "Fl": {
+        "name": "Flerovium"
+    },
+    "Mc": {
+        "name": "Moscovium"
+    },
+    "Lv": {
+        "name": "Livermorium"
+    },
+    "Ts": {
+        "name": "Tennessine"
+    },
+    "Og": {
+        "name": "Oganesson"
+    },
+    "La": {
+        "name": "Lanthanum"
+    },
+    "Ce": {
+        "name": "Cerium"
+    },
+    "Pr": {
+        "name": "Praseodymium"
+    },
+    "Nd": {
+        "name": "Neodymium"
+    },
+    "Pm": {
+        "name": "Promethium"
+    },
+    "Sm": {
+        "name": "Samarium"
+    },
+    "Eu": {
+        "name": "Europium"
+    },
+    "Gd": {
+        "name": "Gadolinium"
+    },
+    "Tb": {
+        "name": "Terbium"
+    },
+    "Dy": {
+        "name": "Dysprosium"
+    },
+    "Ho": {
+        "name": "Holmium"
+    },
+    "Er": {
+        "name": "Erbium"
+    },
+    "Tm": {
+        "name": "Thulium"
+    },
+    "Yb": {
+        "name": "Ytterbium"
+    },
+    "Ac": {
+        "name": "Actinium"
+    },
+    "Th": {
+        "name": "Thorium"
+    },
+    "Pa": {
+        "name": "Protactinium"
+    },
+    "U": {
+        "name": "Uranium"
+    },
+    "Np": {
+        "name": "Neptium"
+    },
+    "Pu": {
+        "name": "Plutonium"
+    },
+    "Am": {
+        "name": "Americium"
+    },
+    "Cm": {
+        "name": "Curium"
+    },
+    "Bk": {
+        "name": "Berkelium"
+    },
+    "Cf": {
+        "name": "Californium"
+    },
+    "Es": {
+        "name": "Einsteinium"
+    },
+    "Fm": {
+        "name": "Fermium"
+    },
+    "Md": {
+        "name": "Mendelevium"
+    },
+    "No": {
+        "name": "Nobelium"
     }
 }
